c441f5eba690c690eee1ac59c61d2695d12b56ba
nikolascoelho <nikolasgianoglou.coelho-ext@ab-inbev.com>
2023-07-14 14:42:42 -0300

    move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Nikolas Gianoglou Coelho on 14/07/2023.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?)
+}
